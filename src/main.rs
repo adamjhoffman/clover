d074729fabba6b831a9cddfb0ced645096130875
Adam Hoffman <adamjosephhoffman@icloud.com>
2024-09-16 16:15:55 +0200

    implemented the time functionality and improved the console printing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashMap,
+    cmp::max,
     fs::{read_to_string, File, OpenOptions},
     io::Write,
     path::{Path, PathBuf},
@@ -7,8 +7,200 @@ use std::{
 
 use clap::{arg, command, value_parser, Command};
 
+#[derive(serde::Deserialize, serde::Serialize)]
+struct ClassOverview<'a> {
+    overview: Vec<Vec<Vec<bool>>>,
+    #[serde(borrow)]
+    class_names: Vec<&'a str>,
+    #[serde(borrow)]
+    task_names: Vec<&'a str>,
+}
+
+impl<'a> ClassOverview<'a> {
+    fn new(configuration: &'a str) -> ClassOverview {
+        if !configuration.is_empty() {
+            if let Ok(overview) = serde_json::from_str::<ClassOverview>(&configuration) {
+                return overview;
+            } else {
+                println!("Failed to parse config file!");
+            }
+        }
+        return ClassOverview {
+            overview: Vec::new(),
+            class_names: Vec::new(),
+            task_names: Vec::new(),
+        };
+    }
+    fn generate_empty_class(task_count: &usize) -> Vec<bool> {
+        vec![false; *task_count]
+    }
+    fn generate_empty_week(class_count: &usize, task_count: &usize) -> Vec<Vec<bool>> {
+        vec![vec![false; *task_count]; *class_count]
+    }
+    fn push_class(&mut self, class_name: &'a str) {
+        self.class_names.push(class_name);
+        for classes in &mut self.overview {
+            classes.push(Self::generate_empty_class(&self.task_names.len()))
+        }
+    }
+    fn push_task(&mut self, task_name: &'a str) {
+        self.task_names.push(task_name);
+        for classes in &mut self.overview {
+            for tasks in classes {
+                tasks.push(false);
+            }
+        }
+    }
+    fn set_time_frame(&mut self, week_count: &usize) {
+        if self.overview.len() > *week_count {
+            self.overview.truncate(*week_count);
+        } else {
+            for _ in self.overview.len()..*week_count {
+                self.overview.push(Self::generate_empty_week(
+                    &self.class_names.len(),
+                    &self.task_names.len(),
+                ))
+            }
+        }
+    }
+    fn complete_task_for_class(
+        &mut self,
+        week: &usize,
+        class_name: &str,
+        task_name: &'a str,
+    ) -> Result<(), &'static str> {
+        if *week < self.overview.len() {
+            if let Some(class_name_index) = self.class_names.iter().position(|&c| c == class_name) {
+                if let Some(task_name_index) = self.task_names.iter().position(|&t| t == task_name)
+                {
+                    self.overview[*week][class_name_index][task_name_index] = true;
+                    return Ok(());
+                }
+                return Err("Invalid task supplied!");
+            }
+            return Err("Invalid class supplied!");
+        }
+        return Err("Invalid week supplied!");
+    }
+    fn save_configuration(&self, configuration_file_path: &Path) {
+        if let Ok(mut configuration_file) = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .truncate(true)
+            .open(configuration_file_path)
+        {
+            configuration_file
+                .write(
+                    serde_json::to_string(&self)
+                        .expect("Failed to parse current configuration!")
+                        .as_bytes(),
+                )
+                .expect("Failed to write to config file!");
+        } else {
+            println!("Failed to open config file and save current configuration!")
+        }
+    }
+}
+
+impl<'a> std::fmt::Display for ClassOverview<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let first_column_width = " week ".len() + self.overview.len().to_string().len() + " ".len();
+
+        let total_task_name_length = self
+            .task_names
+            .iter()
+            .map(|c| " ".len() + c.len() + " ".len())
+            .sum::<usize>()
+            + max(self.task_names.len() - 1, 0) * "|".len();
+        let column_widths = self
+            .class_names
+            .iter()
+            .map(|c| max(c.len() + 2, total_task_name_length))
+            .collect::<Vec<usize>>();
+
+        let total_length = first_column_width
+            + column_widths.iter().sum::<usize>()
+            + (column_widths.len() + 1) * "|".len();
+
+        write!(f, "{}\n", "-".repeat(total_length))?;
+        write!(f, "{}|", " ".repeat(first_column_width))?;
+
+        for (index, class_name) in self.class_names.iter().enumerate() {
+            write!(
+                f,
+                " {}{}|",
+                class_name,
+                " ".repeat(column_widths[index] - class_name.len() - " ".len()),
+            )?;
+        }
+        write!(f, "\n")?;
+
+        write!(
+            f,
+            "{}{}\n",
+            " ".repeat(first_column_width),
+            "-".repeat(total_length - (first_column_width))
+        )?;
+        write!(f, "{}|", " ".repeat(first_column_width))?;
+        for column_index in 0..self.class_names.len() {
+            let mut current_width = 0;
+            for (index, task_name) in self.task_names.iter().enumerate() {
+                current_width += format!(" {task_name} ").len();
+                write!(
+                    f,
+                    " {}{}|",
+                    task_name,
+                    " ".repeat(if index < (self.task_names.len() - 1) {
+                        1
+                    } else {
+                        (column_widths[column_index] - current_width) - 1
+                    })
+                )?;
+            }
+        }
+        write!(f, "\n")?;
+
+        for (week, classes) in self.overview.iter().enumerate() {
+            write!(f, "{}\n", "-".repeat(total_length))?;
+            write!(
+                f,
+                " week {}{week} |",
+                "0".repeat(self.overview.len().to_string().len() - format!("{week}").len())
+            )?;
+            for (column_index, states) in classes.iter().enumerate() {
+                let mut current_width = 0;
+                for (index, task_name) in self.task_names.iter().enumerate() {
+                    current_width += format!(" {task_name} ").len();
+                    write!(
+                        f,
+                        " {}{}|",
+                        {
+                            if states[index] {
+                                "█"
+                            } else {
+                                " "
+                            }
+                        }
+                        .repeat(task_name.len()),
+                        " ".repeat(if index < (self.task_names.len() - 1) {
+                            1
+                        } else {
+                            (column_widths[column_index] - current_width) - 1
+                        })
+                    )?;
+                }
+            }
+            write!(f, "\n")?;
+        }
+
+        write!(f, "{}", "-".repeat(total_length))?;
+
+        Ok(())
+    }
+}
+
 fn main() {
-    let config_file_path = home::home_dir()
+    let configuration_file_path = home::home_dir()
         .expect("Failed to find home directory")
         .join(".clover");
     let matches = command!()
@@ -22,12 +214,17 @@ fn main() {
         .subcommand(
             Command::new("addclass")
                 .about("Adds a new class to the overview")
-                .arg(arg!([name] "The class name")),
+                .arg(arg!([class_name] "The class name").value_parser(value_parser!(String))),
         )
         .subcommand(
             Command::new("addtask")
                 .about("Adds a new task to the overview")
-                .arg(arg!([name] "The task name")),
+                .arg(arg!([task_name] "The task name").value_parser(value_parser!(String))),
+        )
+        .subcommand(
+            Command::new("settime")
+                .about("Sets the amount of weeks the overview is supposed to track")
+                .arg(arg!([week_count] "The week count").value_parser(value_parser!(usize))),
         )
         .subcommand(Command::new("show").about("Shows the class overview"))
         .subcommand(
@@ -35,7 +232,7 @@ fn main() {
                 .about("Completes the task for the given class")
                 .arg(
                     arg!(
-                        -c --class <NAME> "The class to complete the task for"
+                        -c --class <CLASS> "The class to complete the task in"
                     )
                     .required(true)
                     .value_parser(value_parser!(String)),
@@ -46,6 +243,13 @@ fn main() {
                     )
                     .required(true)
                     .value_parser(value_parser!(String)),
+                )
+                .arg(
+                    arg!(
+                        -w --week <WEEK> "The week to complete the task for"
+                    )
+                    .required(true)
+                    .value_parser(value_parser!(usize)),
                 ),
         )
         .get_matches();
@@ -57,97 +261,66 @@ fn main() {
             config_file_path.display()
         ))
     } else {
-        if !config_file_path.exists() {
-            File::create(&config_file_path).expect(
+        if !configuration_file_path.exists() {
+            File::create(&configuration_file_path).expect(
                 "Failed to create config file, does the directory exist or is it read-only?",
             );
         }
-        read_to_string(&config_file_path)
+        read_to_string(&configuration_file_path)
             .expect("Failed to read config file, is the directory read-only?")
     };
 
-    let mut overview = if config.is_empty() {
-        HashMap::new()
-    } else {
-        if let Ok(overview) =
-            serde_json::from_str::<HashMap<String, HashMap<String, bool>>>(&config)
-        {
-            overview
-        } else {
-            println!("Failed to parse config file!");
-            HashMap::new()
-        }
-    };
+    let mut class_overview = ClassOverview::new(&config);
 
     if let Some(matches) = matches.subcommand_matches("addclass") {
-        if let Some(name) = matches.get_one::<String>("name") {
-            println!("Added class {name}");
-            overview.insert(name.to_owned(), HashMap::new());
-            save_current_configuration(&overview, &config_file_path);
+        if let Some(class_name) = matches.get_one::<String>("class_name") {
+            println!("Added class {class_name}");
+            class_overview.push_class(class_name);
         }
     }
 
     if let Some(matches) = matches.subcommand_matches("addtask") {
-        if let Some(name) = matches.get_one::<String>("name") {
-            println!("Added task {name}");
-            for (_, tasks) in &mut overview {
-                tasks.insert(name.to_owned(), false);
-            }
-            save_current_configuration(&overview, &config_file_path);
+        if let Some(task_name) = matches.get_one::<String>("task_name") {
+            println!("Added task {task_name}");
+            class_overview.push_task(task_name);
+        }
+    }
+
+    if let Some(matches) = matches.subcommand_matches("settime") {
+        if let Some(week_count) = matches.get_one::<usize>("week_count") {
+            println!("Set week count to {week_count}");
+            class_overview.set_time_frame(week_count);
         }
     }
 
     if let Some(matches) = matches.subcommand_matches("complete") {
-        if let Some(class) = matches.get_one::<String>("class") {
-            if let Some(task) = matches.get_one::<String>("task") {
-                println!("Completing {task} for {class}");
-                if let Some(tasks) = overview.get_mut(class) {
-                    if tasks.contains_key(task) {
-                        tasks.insert(task.to_owned(), true);
-                        save_current_configuration(&overview, &config_file_path);
+        if let Some(week) = matches.get_one::<usize>("week") {
+            if let Some(class_name) = matches.get_one::<String>("class") {
+                if let Some(task_name) = matches.get_one::<String>("task") {
+                    if let Err(error) =
+                        class_overview.complete_task_for_class(week, class_name, task_name)
+                    {
+                        println!("Failed to completed {task_name} for {class_name} in week {week}: {error}");
                     } else {
-                        println!(
-                            "Invalid task supplied! Current tasks: {:?}",
-                            overview[class].keys()
-                        );
+                        println!("Completed {task_name} for {class_name} in week {week}");
                     }
                 } else {
-                    println!(
-                        "Invalid class supplied! Current classes: {:?}",
-                        overview.keys()
-                    );
+                    println!("No task supplied, the task is necessary to flag its completion!");
                 }
             } else {
-                println!("No task supplied, the task is necessary to flag its completion!");
+                println!(
+                    "No class supplied, the class is necessary to flag the task's completion!"
+                );
             }
         } else {
-            println!("No class supplied, the class is necessary to flag the task's completion!");
+            println!("No week supplied, the week is necessary to flag the task's completion!");
         }
     }
 
     if let Some(_) = matches.subcommand_matches("show") {
-        println!("{overview:?}");
+        println!("{class_overview}");
         return;
     }
-}
 
-fn save_current_configuration(
-    overview: &HashMap<String, HashMap<String, bool>>,
-    config_file_path: &Path,
-) {
-    if let Ok(mut config_file) = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(config_file_path)
-    {
-        config_file
-            .write(
-                serde_json::to_string(&overview)
-                    .expect("Failed to parse current configuration!")
-                    .as_bytes(),
-            )
-            .expect("Failed to write to config file!");
-    } else {
-        println!("Failed to open config file and save current configuration!")
-    }
+    class_overview.save_configuration(&configuration_file_path);
 }
